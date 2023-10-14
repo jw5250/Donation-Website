0eff11f9611a86eea6ece63cc099363e15b69556
Ricky Yang <ry8467@rit.edu>
2023-10-14 15:38:00 -0400

    initializing FundingBasket

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/FundingBasketController.java
@@ -0,0 +1,166 @@
+package com.ufund.api.ufundapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.ufund.api.ufundapi.persistence.FundingBasketFileDAO;
+import com.ufund.api.ufundapi.persistence.NeedDAO;
+
+import com.ufund.api.ufundapi.model.Need;
+
+/**
+ * Handles the REST API requests for the Need resource
+ * <p>
+ * {@literal @}RestController Spring annotation identifies this class as a REST API
+ * method handler to the Spring framework
+ * 
+ */
+
+@RestController
+@RequestMapping("FundingBasket")
+public class FundingBasketController {
+    private static final Logger LOG = Logger.getLogger(NeedsController.class.getName());
+    private FundingBasketFileDAO fundingBasketDAO;
+
+    /**
+     * Creates a REST API controller to reponds to requests
+     * 
+     * @param fundingBasketDAO The {@link FundingBasketFileDAO fundingBasketDAO Data Access Object} to perform CRUD operations
+     * <br>
+     * This dependency is injected by the Spring Framework
+     */
+    public FundingBasketController(FundingBasketFileDAO fundingBasketDAO) {
+        this.fundingBasketDAO = fundingBasketDAO;
+    }
+    /**
+     * Responds to the GET request for a {@linkplain Need need} for the given name
+     * 
+     * @param name The name used to locate the {@link Need need}
+     * 
+     * @return ResponseEntity with {@link Need need} object and HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/{name}")
+    public ResponseEntity<Need> getNeed(@PathVariable String name) {
+        LOG.info("GET /fundingbasket/" + name);
+        try {
+            Need need = fundingBasketDAO.getNeed(name);
+            if (need != null)
+                return new ResponseEntity<Need>(need,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain Need needs}
+     * 
+     * @return ResponseEntity with array of {@link Need need} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("")
+    public ResponseEntity<Need[]> getCupboard() {
+        LOG.info("GET /fundingbasket");
+        try{
+            Need[] needs = fundingBasketDAO.getCupboard();
+            return new ResponseEntity<Need[]>(needs, HttpStatus.OK);
+        }catch(IOException e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain Need needs} whose name contains
+     * the text in name
+     * 
+     * @param name The name parameter which contains the text used to find the {@link Need need}
+     * 
+     * @return ResponseEntity with array of {@link Need need} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * <p>
+     */
+    @GetMapping("/")
+    public ResponseEntity<Need[]> searchNeeds(@RequestParam String name) {
+        LOG.info("GET /fundingbasket/?name="+name);
+        try{
+            Need[] needs = fundingBasketDAO.searchNeeds(name);
+            return new ResponseEntity<Need[]>(needs, HttpStatus.OK);
+        }catch(IOException e){
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    /**
+     * Creates a {@linkplain Need need} with the provided need object
+     * 
+     * @param need - The {@link Need need} to create
+     * 
+     * @return ResponseEntity with created {@link Need need} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link Need need} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+   @PostMapping("")
+    public ResponseEntity<Need> createNeed(@RequestBody Need need) {
+        LOG.info("POST /fundingbasket " + need);
+        try{
+            System.out.println(need.toString());//Need to add something that checks if a need's name exists.
+
+            Need newNeed = fundingBasketDAO.createNeed(need);
+            if(newNeed == null){
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }else{
+                return new ResponseEntity<Need>(newNeed, HttpStatus.CREATED);
+            }
+
+        }catch(IOException e){
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Deletes a {@linkplain Need need} with the given name
+     * 
+     * @param name The name of the {@link Need need} to deleted
+     * 
+     * @return ResponseEntity HTTP status of OK if deleted<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Need> deleteNeed(@PathVariable String name) {
+        LOG.info("DELETE /fundingbasket/" + name);
+        try{
+            boolean needFound = fundingBasketDAO.deleteNeed(name);
+            if(needFound == true){
+                return new ResponseEntity<>(HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+        }catch(IOException e){
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
