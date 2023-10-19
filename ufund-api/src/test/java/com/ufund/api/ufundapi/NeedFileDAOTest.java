943b23f3df13c78b6fc6c512c9762b43f6b31406
dra7835 <dra7835@rit.edu>
2023-10-18 23:01:29 -0400

    Added testing methods for GetCupboard both with and without a search term.

--- ufund-api/src/test/java/com/ufund/api/ufundapi/NeedFileDAOTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/NeedFileDAOTest.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -25,11 +27,41 @@ public class NeedFileDAOTest {
     Need[] testNeeds;
     ObjectMapper mockObjectMapper;
     @BeforeEach
-    public void setupNeedFileDAO() throws IOException {}
+    public void setupNeedFileDAO() throws IOException {
+        mockObjectMapper = mock(ObjectMapper.class);
+
+        Need[] testNeeds = new Need[3];
+        testNeeds[0] = new Need("Art", "funding", 10000, 5);
+        testNeeds[1] = new Need("Teachers", "volunteer", 0, 10);
+        testNeeds[2] = new Need("null", "null", 0, 0);
+
+        File dummy = new File("none.txt");
+        when(mockObjectMapper
+            .readValue("none.txt",Need[].class))
+                .thenReturn(testNeeds);
+        needFileDAO = new NeedFileDAO("none.txt",mockObjectMapper);
+    }   
     @Test
-    public void testGetNeeds(){}
+    public void testGetCupboardArray() throws IOException{
+        Need[] testArray = new Need[3];
+        testArray[0] = new Need("Art", "funding", 10000, 5);
+        testArray[1] = new Need("Teachers", "volunteer", 0, 10);
+        testArray[2] = new Need("null", "null", 0, 0);
+
+        Need[] testResult = needFileDAO.getDataArray();
+
+        assertArrayEquals(testArray, testResult, "testGetCupboardArray");
+    }
     @Test
-    public void testFindNeeds() {}
+    public void testSearchCupboard() throws IOException{
+        Need[] testArray = new Need[2];
+        testArray[0] = new Need("Art", "funding", 10000, 5);
+        testArray[1] = new Need("Teachers", "volunteer", 0, 10);
+
+        Need[] testResult = needFileDAO.searchDataArray("r");
+
+        assertArrayEquals(testArray, testResult, "testGetCupboardArray");
+    }
     @Test
     public void testGetNeed() {}
     @Test
