4d86a96f6b0a434f17b96ac97038c462d0e80f7f
jw5250 <jw5250@rit.edu>
2023-10-20 14:45:45 -0400

    Added frontend interfaces pertaining to Ufund manager and Helper, connected them to the main application.

--- ufund-ui/src/app/app.module.ts
+++ ufund-ui/src/app/app.module.ts
@@ -2,29 +2,45 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './needs/needs.component';
-//import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { NeedsComponent } from './needs/needs.component';
+import { NeedDetailComponent } from './need-detail/need-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
-//import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-//import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { NeedSearchComponent } from './need-search/need-search.component';
+import { HelperScreenComponent } from './accountSystemFrontend/helper-screen/helper-screen.component';
+import { ManagerScreenComponent } from './accountSystemFrontend/manager-screen/manager-screen.component';
+import { SigninLoginScreenComponent } from './accountSystemFrontend/signin-login-screen/signin-login-screen.component';
+
+
+//For testing the database.
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { UserDatabaseTestService } from './user-database-test.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    //HeroDetailComponent,
+    NeedsComponent,
+    NeedDetailComponent,
     MessagesComponent,
-    //DashboardComponent,
-    //HeroSearchComponent
+    DashboardComponent,
+    NeedSearchComponent,
+    HelperScreenComponent,
+    ManagerScreenComponent,
+    SigninLoginScreenComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
+    /*
+    HttpClientInMemoryWebApiModule will now simulate a server,
+    preventing the actual server from getting actual requests.
+    */
+    /*HttpClientInMemoryWebApiModule.forRoot(
+    UserDatabaseTestService, { dataEncapsulation: false })*/
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ufund-ui/src/app/need.service.ts
+++ ufund-ui/src/app/need.service.ts
@@ -106,7 +106,7 @@ export class NeedService {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
+      // TODO: better job of transforming error for Need consumption
       this.log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
