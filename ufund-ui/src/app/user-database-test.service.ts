0f79c9777ab488270bce19aff7441cda599e26c1
jw5250 <jw5250@rit.edu>
2023-10-21 17:49:59 -0400

    Removed unnecessary frontend.

--- ufund-ui/src/app/user-database-test.service.ts
+++ ufund-ui/src/app/user-database-test.service.ts
@@ -7,15 +7,7 @@ import { User } from './user';
 export class UserDatabaseTestService implements InMemoryDbService {
   createDb(){
     const users = [
-      { name: 'Dr. Nice', isManager: false},
-      { name: 'Bombasto', isManager: false},
-      { name: 'Celeritas', isManager: false},
-      { name: 'Magneta', isManager: false},
-      { name: 'RubberMan', isManager: false},
-      { name: 'Dynama', isManager: false},
-      { name: 'Dr. IQ', isManager: true},
-      { name: 'Magma', isManager: false},
-      { name: 'Tornado', isManager: false}
+      {"name":"Bob", "isManager":false, "fundingBasket":[]}
     ];
     return {users};
   }
